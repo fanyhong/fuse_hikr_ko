64d12e1392434ec37a86d5ab79f19937c7e8e0c3
HHH <hhh@HHHui-MacBook-Pro.local>
2017-02-09 13:14:36 +0900

    라우터를 사용하여 각 페이지에 디펜던시 라우터를 적용

--- Pages/EditHikePage.js
+++ Pages/EditHikePage.js
@@ -2,7 +2,8 @@
 // require() 로 FuseJS의 Observable 모듈을 가져옴
 var Observable = require("FuseJS/Observable");
 
-var hike = Observable();
+// Parameter Observable 을 전달받아 hike 에 할당 (router 를 통해 이동시 함께 전달받은 인자)
+var hike = this.Parameter;
 
 // hike.value 에 값이 들어오면 해당 값의 name, location 등을 리턴하는 함수를 map()에 전달하여 Observable 처럼 양방향 데이터 바인딩
 var name = hike.map(function(x) { return x.name; });
@@ -11,6 +12,10 @@ var distance = hike.map(function(x) { return x.distance; });
 var rating = hike.map(function(x) { return x.rating; });
 var comments = hike.map(function(x) { return x.comments; });
 
+function goBack(){
+    router.goBack();    // router.goBack() 을 사용하여 뒤로가기 (이전에 push로 이동했기때문에 가능)
+};
+
 // 외부에서 접근이 가능하도록 내보내기
 module.exports = {
     name: name,
@@ -18,4 +23,5 @@ module.exports = {
     distance: distance,
     rating: rating,
     comments: comments,
+    goBack: goBack
 };
--- Pages/HomePage.js
+++ Pages/HomePage.js
@@ -1,14 +1,16 @@
 var hikes = require("hikes"); // hikes.js 를 import
 
 // Each 안의 버튼이 클릭되면 호출 (이때 arg 를 인수로 받을 수 있음)
-function chooseHike(arg){
-    console.log(arg);
-    hike.value = arg.data;  // arg.data 는 Each 에서 선택된 데이터
+function goHike(arg){
+    // console.log(arg);
+    var hike = arg.data;  // arg.data 는 Each 에서 선택된 데이터
+ 
+    router.push("editHike", hike);    // router.push() 로 원하는 경로로 이동, 두번째 인자를 통해 hike를 함께 전달하면 이동된 페이지에서 this.Parameter 로 접근하여 사용
 };
 
 // 외부에서 접근이 가능하도록 내보내기
 module.exports = {
     hikes: hikes,
 
-    chooseHike: chooseHike
+    goHike: goHike
 };
